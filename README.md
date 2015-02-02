1c56a2aab8248294c15438ddc3105951aa7662e0
7yan00 <7yan00@7yan00-no-MacBook-Pro.local>
2015-02-02 17:51:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#simple web-image clowler by golang
+
+this is simple web-image clawler written by golang!
+
+##Usage
+
+This is example:
+```sh
+
+go run main.go http://blog.golang.org/
+
+```
+
+results:
+
+you will got same .img Picture! Yeah!
+
+Enjoy Web-Clawl life!!!!!!!!!!
---
+++ clawler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"flag"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func GetPage(base string) []*url.URL {
+	doc, _ := goquery.NewDocument(base)
+	var result []*url.URL
+	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+		target, _ := s.Attr("src")
+		base,_ := url.Parse(base)
+		targets, _ := url.Parse(target)
+		result = append(result, base.ResolveReference(targets))
+	})
+	return result
+}
+
+func GetImage(urls []*url.URL) {
+	for i, url := range urls {
+		urrl := url.String()
+		response, err := http.Get(urrl)
+		if err != nil {
+			panic(err)
+		}
+
+		defer response.Body.Close()
+
+		file, err := os.Create(fmt.Sprintf("hoge%d.jpg", i))
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		io.Copy(file, response.Body)
+	}
+}
+
+func main() {
+	flag.Parse()
+	fmt.Println(It works!)
+	base := flag.Arg(0)
+	urls := (GetPage(base))
+	GetImage(urls)
+	fmt.Println(fin! You ought to get some picture!)
+}
